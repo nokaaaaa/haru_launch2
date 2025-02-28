5545a8c9b6d6a9e0c3841e13976d63a24c6fbcae
Noka Yuki <88697291+nokaaaaa@users.noreply.github.com>
2025-02-28 10:40:46 +0900

    Update haru.launch.py

--- launch/haru.launch.py
+++ launch/haru.launch.py
@@ -7,11 +7,17 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare  
 
+#メモ 自己位置推定において最初の位置を(0,0)となるように壁の距離を決めて、自己位置のとり方が変わるところでも前の自己位置から壁の距離を決定してあげる
+
 def generate_launch_description():
     ld = LaunchDescription()
 
     DeclareLaunchArgument('field_color', default_value='red',
                           description='Color of the field')
+    
+    ld.add_action(DeclareLaunchArgument('field_color', default_value='red', description='Color of the field'))
+    
+    
 
     hransac_config_path = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "..", "config", "hransac_config.yaml"
@@ -46,20 +52,21 @@ def generate_launch_description():
         output="screen",
         parameters=[pid_config_path, {'field_color': LaunchConfiguration('field_color')}]
     )
-    '''
-    # LiDAR ノード
-    sick_picoscan_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([
-                FindPackageShare("sick_scan_xd"), "launch", "sick_picoscan.launch.py"
-            ])
-        ),
+
+    sick_scan_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            FindPackageShare('sick_scan_xd'), '/launch/sick_picoscan.launch.py'
+        ]),
         launch_arguments={
             "hostname": "192.168.12.6",
-            "udp_receiver_ip": "192.168.12.1"
-        }.items()
+            "udp_receiver_ip":"192.168.12.1",
+            "all_segments_min_deg": "-90",
+            "all_segments_max_deg": "90",
+         }.items()
     )
-   '''
+    
+    
+   
     # gui ノード
     gui_node = Node(
         package="haru_gui",
@@ -103,9 +110,9 @@ def generate_launch_description():
 
     ld.add_action(hransac_node)
     ld.add_action(pid_node)
-    #ld.add_action(sick_picoscan_launch) 
     ld.add_action(gui_node)
     ld.add_action(od_node)
+    ld.add_action(sick_scan_launch)
     ld.add_action(op_node)
     ld.add_action(rogilink_node)
     ld.add_action(example_node)
